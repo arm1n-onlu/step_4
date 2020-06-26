0e803c066cf9d1896de92603257a375495e7c4b4
arm1n-onlu <ibart.6@gmail.com>
2020-06-26 22:31:58 +0300

    write your message

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="es",
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: es,ru or en")
 
 
@@ -21,4 +21,3 @@ def browser(request):
     time.sleep(10)
     print("\nquit browser..")
     browser.quit()
-
