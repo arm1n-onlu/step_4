39e21986732f94b6034673dbb50c9d3c7b5ef1d5
artur.puchkov <Artur.Puchkov@russianpost.ru>
2022-03-27 16:33:15 +0300

    add_check_for_4_task

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,8 +3,17 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
+    def __init__(self, browser, url, timeout=10):
+        super().__init__(browser, url, timeout)
+
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(By.ID, "login_link")
         login_link.click()
+
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(By.ID, "login_link_invalid"), "Login link is not presented"
+
+    def add_to_busket(self):
+        busket_link = self.browser.find_element(By.ID, "add_to_basket_form")
+        busket_link.click()
+
