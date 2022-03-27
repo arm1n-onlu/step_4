96393c56897416ed569efb985736ab7ae06f004a
artur.puchkov <Artur.Puchkov@russianpost.ru>
2022-03-27 18:49:56 +0300

    add_param_tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,14 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('num', [*range(1, 7), pytest.param(7, marks=pytest.mark.xfail), *range(8, 10)])
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
     page = ProductPage(browser, link)
     page.open()
     page.press_button_add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_message_about_adding()
     page.should_be_message_basket_total()
-
